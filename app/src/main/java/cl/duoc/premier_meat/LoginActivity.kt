62492f5acd2736fbf69d1b48acb4c45c263c0f12
Kzu <alvaro.vega.pavez@gmail.com>
2024-09-09 22:30:10 -0300

    se implementa vista login con jetpack compose

--- app/src/main/java/cl/duoc/premier_meat/LoginActivity.kt
+++ app/src/main/java/cl/duoc/premier_meat/LoginActivity.kt
@@ -2,43 +2,23 @@ package cl.duoc.premier_meat
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import cl.duoc.premier_meat.model.User
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
-        val login: Button = findViewById(R.id.btnLogin)
-        val backButton: Button = findViewById(R.id.btnBack)
-
-        login.setOnClickListener {
-            val email = findViewById<EditText?>(R.id.editTextEmail).text.toString()
-            val password = findViewById<EditText>(R.id.editTextPassword).text.toString()
-
-            if (!validateEditText(email, password)) {
-                login(email, password)
-            } else {
-                Toast.makeText(this, "Ingresa los campos antes de ingresar", Toast.LENGTH_SHORT).show()
-            }
-        }
-
-        backButton.setOnClickListener {
-            goHome()
+        setContent {
+            LoginScreen(
+                onLoginClick = { email, password ->  // Pasar los parámetros email y password
+                    login(email, password)
+                },
+                onBackClick = { goHome() }
+            )
         }
     }
 
@@ -47,12 +27,8 @@ class LoginActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
-    private fun validateEditText(email: String, password: String): Boolean {
-        return email == "" || password == ""
-    }
-
     private fun login(email: String, password: String) {
-        val user = finUser(email, password)
+        val user = findUser(email, password)
 
         if (user != null) {
             if (user.admin) {
@@ -65,7 +41,7 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-    private fun finUser(email: String, password: String): User? {
+    private fun findUser(email: String, password: String): User? {
         return MainActivity.UserData.userList.find {
             it.email == email && it.password == password
         }
