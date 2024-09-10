16a7b6ebba56a82a4f86e8910efbd80b6c26123e
Kzu <alvaro.vega.pavez@gmail.com>
2024-09-09 23:06:32 -0300

    se implementa vista menu con jetpack compose

--- app/src/main/java/cl/duoc/premier_meat/MenuActivity.kt
+++ app/src/main/java/cl/duoc/premier_meat/MenuActivity.kt
@@ -3,54 +3,31 @@ package cl.duoc.premier_meat
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import cl.duoc.premier_meat.model.User
 
-class MenuActivity : AppCompatActivity() {
+class MenuActivity : ComponentActivity() {
 
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_menu)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
         val user = intent.getParcelableExtra<User>("user", User::class.java)
-        val changePasswordButton: Button = findViewById(R.id.btnChangePassword)
-        val backButton: Button = findViewById(R.id.btnBack)
-        var userBackup: String = ""
+        val userBackup: String = user?.name ?: "Usuario"
 
-        if (user != null && user.name != "") {
-            userBackup = user.name
+        setContent {
+            MenuScreen(
+                userName = userBackup,
+                onChangePasswordClick = {
+                    goToChangePassword(user)
+                },
+                onBackClick = {
+                    goToBack()
+                }
+            )
         }
-
-        if (user != null && user.name != "") {
-            welcome(user.name)
-        } else {
-            welcome(userBackup)
-        }
-
-        changePasswordButton.setOnClickListener {
-            goToChangePassword(user)
-        }
-
-        backButton.setOnClickListener {
-            goToBack()
-        }
-    }
-
-    private fun welcome(name: String) {
-        findViewById<TextView>(R.id.textWelcome).text = "Bienvenido ${name}, que te gustaría hacer?"
     }
 
     private fun goToChangePassword(user: User?) {
